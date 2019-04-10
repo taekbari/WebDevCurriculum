3b4003dc8c4587fbcfc9c7b61023947889d6774f
taekbari <taekbari157@gmail.com>
2019-04-10 16:39:38 +0900

    Use Ajax to create GET/POST request.

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -2,10 +2,19 @@ const express = require('express'),
 	path = require('path'),
 	app = express();
 
+app.use( express.urlencoded() );
 app.use(express.static('client'));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/foo', (req, res) => {
+	res.send( req.query.bar );
+});
+
+app.post('/foo', (req, res) => {
+	res.send( req.body.bar );
 });
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
