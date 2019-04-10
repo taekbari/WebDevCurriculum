1050fa018a127ff4075350ff3cdfb91320281506
taekbari <taekbari157@gmail.com>
2019-04-10 18:40:27 +0900

    Add file list request and response

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,6 +1,7 @@
-const express = require('express'),
-	path = require('path'),
-	app = express();
+const express = require('express');
+const path = require('path');
+const fileSystem = require( 'fs' );
+const app = express();
 
 app.use( express.urlencoded() );
 app.use(express.static('client'));
@@ -15,6 +16,17 @@ app.get('/foo', (req, res) => {
 
 app.post('/foo', (req, res) => {
 	res.send( req.body.bar );
+});
+
+app.get('/files', (req, res) => {
+	fileSystem.readdir(path.join(__dirname, 'files'), (error, fileList) => {
+		if ( error ) {
+			res.status( 500 ).send( {status: 500, message: 'internal error', type: 'internal'} );
+			return;
+		}
+		
+		res.json( {list: fileList} );
+	});
 });
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
